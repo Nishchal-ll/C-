c98189c47aabe467a0f1f6505d8dea6d910b6269
Nishchal-ll <acharyanischal2004@gmail.com>
2025-07-04 10:44:03 +0545

    C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration.Assemblies;
+
+class Add
+{
+    public int sum;
+    public Add(int x, int y)
+    {
+        sum = x + y;
+        Console.WriteLine("The sum is {0}", sum);
+    }
+
+    static void Main(string[] args)
+    {
+        int number1 = 2;
+        int number2 = 3;
+
+        Add addition = new Add(number1, number2);
+    }
+}
+
+
+
+
+
+
+
+    
+
+
+
+
